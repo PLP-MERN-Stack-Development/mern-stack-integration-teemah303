43acda043e7debf1b0adbfd241881938ee21ee1d
teemah303 <amir.m1700769@st.futminna.edu.ng>
2025-11-11 11:28:33 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import PostPage from './pages/PostPage';
+import NewPost from './pages/NewPost';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<PostPage />} />
+          <Route path="/new" element={<NewPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
